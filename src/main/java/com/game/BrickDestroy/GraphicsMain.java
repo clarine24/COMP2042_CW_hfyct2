f18c1d948a3ca814dcc3e77032150c174e261877
clarine24 <tan.ckaili01@gmail.com>
2021-11-30 23:20:52 +0800

    Create home menu UI

---
+++ src/main/java/com/game/BrickDestroy/GraphicsMain.java
@@ -0,0 +1,23 @@
+package com.game.BrickDestroy;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GraphicsMain extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GraphicsMain.class.getResource("HomeMenuView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Brick Destroy");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
