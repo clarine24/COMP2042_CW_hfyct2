139514e251ed2f83b6abf290632b701fc896ce98
clarine24 <tan.ckaili01@gmail.com>
2021-12-01 16:15:13 +0800

    Show info menu UI when info button clicked

--- src/main/java/com/game/BrickDestroy/HomeMenuController.java
+++ src/main/java/com/game/BrickDestroy/HomeMenuController.java
@@ -2,18 +2,17 @@ package com.game.BrickDestroy;
 
 import javafx.fxml.FXML;
 
-public class HomeMenuController {
-    @FXML
-    private void initialize() {}
+import java.io.IOException;
 
+public class HomeMenuController {
     @FXML
     private void startButtonClicked() {
         System.out.println("button start test pass");
     }
 
     @FXML
-    private void infoButtonClicked() {
-        System.out.println("button info test pass");
+    private void infoButtonClicked() throws IOException {
+        Stages.setRoot("InfoMenuView");
     }
 
     @FXML
