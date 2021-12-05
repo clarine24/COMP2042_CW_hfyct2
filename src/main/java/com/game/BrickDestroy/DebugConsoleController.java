c93c6e7ba253eb3515544ba03f436d2291bc7bff
clarine24 <tan.ckaili01@gmail.com>
2021-12-05 22:24:10 +0800

    Remove next level button on last level

--- src/main/java/com/game/BrickDestroy/DebugConsoleController.java
+++ src/main/java/com/game/BrickDestroy/DebugConsoleController.java
@@ -7,12 +7,20 @@ import javafx.scene.control.Slider;
 public class DebugConsoleController {
     private GameBoardModel model;
 
+    @FXML private Button nextLevel;
     @FXML private Button resetBallCount;
     @FXML private Slider ballSpeed;
 
     @FXML
     private void initialize() {
         model = GameBoardModel.getInstance();
+
+        //Link with wall model
+        model.getWallModel().hasNextLevel().addListener((observableValue, oldValue, newValue) -> {
+            if(! newValue) {
+                nextLevel.setVisible(false);
+            }
+        });
     }
 
     @FXML
