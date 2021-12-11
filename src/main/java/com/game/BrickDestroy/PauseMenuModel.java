4b7db8933fb5ef10bd7ec0c70a009f2ebfe56ede
clarine24 <tan.ckaili01@gmail.com>
2021-12-11 23:20:39 +0800

    Add JavaDoc

--- src/main/java/com/game/BrickDestroy/PauseMenuModel.java
+++ src/main/java/com/game/BrickDestroy/PauseMenuModel.java
@@ -3,12 +3,21 @@ package com.game.BrickDestroy;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 
+/**
+ * The PauseMenuModel is the model for pause menu view.
+ * @author Clarine Tan Kaili (20194533)
+ * @version 2.0
+ * @since 11/12/21
+ */
 public class PauseMenuModel {
     private static PauseMenuModel instance;
 
     private BooleanProperty resume;
     private BooleanProperty restart;
 
+    /**
+     * Creates a new instance of PauseMenuModel.
+     */
     public PauseMenuModel() {
         resume = new SimpleBooleanProperty(false);
         restart = new SimpleBooleanProperty(false);
@@ -16,22 +25,42 @@ public class PauseMenuModel {
         instance = this;
     }
 
+    /**
+     * Gets the created instance of PauseMenuModel.
+     * @return the PauseMenuModel instance
+     */
     public static PauseMenuModel getInstance() {
         return instance;
     }
 
+    /**
+     * Sets the boolean value of restart.
+     * @param restart the new boolean value of restart
+     */
     public void setRestart(boolean restart) {
         this.restart.set(restart);
     }
 
+    /**
+     * Sets the boolean value of resume.
+     * @param resume the new boolean value of resume
+     */
     public void setResume(boolean resume) {
         this.resume.set(resume);
     }
 
+    /**
+     * Gets the boolean property of resume.
+     * @return the boolean property of resume
+     */
     public BooleanProperty isResume() {
         return resume;
     }
 
+    /**
+     * Gets the boolean property of restart.
+     * @return the boolean property of restart
+     */
     public BooleanProperty isRestart() {
         return restart;
     }
