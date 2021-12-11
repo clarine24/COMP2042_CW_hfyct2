409d90920a7d9fb6c8426a5a0c50af9b55b92eb3
clarine24 <tan.ckaili01@gmail.com>
2021-12-11 23:46:50 +0800

    Add JavaDoc

--- src/main/java/com/game/BrickDestroy/SteelBrickModel.java
+++ src/main/java/com/game/BrickDestroy/SteelBrickModel.java
@@ -4,6 +4,13 @@ import javafx.scene.shape.Rectangle;
 
 import java.util.Random;
 
+/**
+ * The SteelBrickModel class is the steel brick model.
+ * This class extends the BrickModel class.
+ * @author Clarine Tan Kaili (20194533)
+ * @version 2.0
+ * @since 11/12/21
+ */
 public class SteelBrickModel extends BrickModel {
     private static final String NAME = "steelBrick";
 
@@ -12,11 +19,18 @@ public class SteelBrickModel extends BrickModel {
 
     private Random rnd;
 
+    /**
+     * Creates a new instance of SteelBrickModel.
+     * @param brick the brick to be created
+     */
     public SteelBrickModel(Rectangle brick) {
         super(brick, NAME, FULL_STRENGTH);
         rnd = new Random();
     }
 
+    /**
+     * Set the effect of an impact on the steel brick.
+     */
     @Override
     public void impact() {
         double x = rnd.nextDouble();
