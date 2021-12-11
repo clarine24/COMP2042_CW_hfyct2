67133a290757491f05f96c3f01f4f904ed8b1ba4
clarine24 <tan.ckaili01@gmail.com>
2021-12-12 00:05:14 +0800

    Add JavaDoc

--- src/main/java/com/game/BrickDestroy/PlayerModel.java
+++ src/main/java/com/game/BrickDestroy/PlayerModel.java
@@ -2,6 +2,12 @@ package com.game.BrickDestroy;
 
 import javafx.scene.shape.Rectangle;
 
+/**
+ * The PlayerModel class is the player model.
+ * @author Clarine Tan Kaili (20194533)
+ * @version 2.0
+ * @since 11/12/21
+ */
 public class PlayerModel {
     private Rectangle playerFace;
     private final double startX;
@@ -13,6 +19,11 @@ public class PlayerModel {
     private final double min;
     private final double max;
 
+    /**
+     * Creates a new instance of PlayerModel.
+     * @param player the player shape to be created
+     * @param wall the wall in which the player is in
+     */
     public PlayerModel(Rectangle player, Rectangle wall) {
         startX = player.getX();
         startY = player.getY();
@@ -25,6 +36,9 @@ public class PlayerModel {
         max = min + wall.getWidth() - playerFace.getWidth();
     }
 
+    /**
+     * Moves the player paddle.
+     */
     public void move() {
         double x = playerFace.getX() + moveAmount;
         if (x < min || x > max) {
@@ -33,23 +47,39 @@ public class PlayerModel {
         playerFace.setX(x);
     }
 
+    /**
+     * Sets the player move amount to the left.
+     */
     public void moveLeft() {
         moveAmount = -DEF_MOVE_AMOUNT;
     }
 
+    /**
+     * Sets the player move amount to the right.
+     */
     public void moveRight() {
         moveAmount = DEF_MOVE_AMOUNT;
     }
 
+    /**
+     * Sets the player move amount to stop.
+     */
     public void stop() {
         moveAmount = 0;
     }
 
+    /**
+     * Moves the player paddle to its starting position.
+     */
     public void moveToStart() {
         playerFace.setX(startX);
         playerFace.setY(startY);
     }
 
+    /**
+     * Gets the player shape.
+     * @return the player shape
+     */
     public Rectangle getPlayerFace() {
         return playerFace;
     }
