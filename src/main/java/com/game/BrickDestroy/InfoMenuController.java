3b063415c52330ae755826fc62bc2ee480a28972
clarine24 <tan.ckaili01@gmail.com>
2021-12-11 21:22:03 +0800

    Add JavaDoc

--- src/main/java/com/game/BrickDestroy/InfoMenuController.java
+++ src/main/java/com/game/BrickDestroy/InfoMenuController.java
@@ -5,7 +5,12 @@ import javafx.scene.control.Label;
 
 import java.io.IOException;
 
-
+/**
+ * The InfoMenuController is the controller for info menu view.
+ * @author Clarine Tan Kaili (20194533)
+ * @version 2.0
+ * @since 11/12/21
+ */
 public class InfoMenuController {
     /**
      * Switch scene to home menu.
