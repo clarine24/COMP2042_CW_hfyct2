16d1a3ea4cdb8265b07f3e695701af80e3314296
clarine24 <tan.ckaili01@gmail.com>
2021-12-11 22:45:37 +0800

    Add JavaDoc

--- src/main/java/com/game/BrickDestroy/GameTimer.java
+++ src/main/java/com/game/BrickDestroy/GameTimer.java
@@ -19,6 +19,10 @@ public class GameTimer extends AnimationTimer {
 
     private int timer;
 
+    /**
+     * Creates an instance of GameTimer with the given wallModel.
+     * @param wall the wallModel
+     */
     public GameTimer(WallModel wall) {
         this.wall = wall;
         running = new SimpleBooleanProperty(false);
@@ -28,12 +32,15 @@ public class GameTimer extends AnimationTimer {
         timer = 0;
     }
 
+    /**
+     * Trigger the events for every timeframe.
+     */
     @Override
     public void handle(long l) {
         wall.move();
         wall.findImpacts();
 
-        if(displayAddBallTextProperty().get()) {
+        if(getDisplayAddBallText().get()) {
             timer++;
             if(timer > 50) {
                 setDisplayAddBallText(false);
@@ -54,34 +61,60 @@ public class GameTimer extends AnimationTimer {
         }
     }
 
+    /**
+     * Start the animation timer.
+     */
     @Override
     public void start() {
         super.start();
         running.setValue(true);
     }
 
+    /**
+     * Stop the animation timer.
+     */
     @Override
     public void stop() {
         super.stop();
         running.setValue(false);
     }
 
+    /**
+     * Gets the boolean property of running.
+     * @return the boolean property of running
+     */
     public BooleanProperty isRunning() {
         return running;
     }
 
+    /**
+     * Gets the boolean property of GameOver.
+     * @return the boolean property of game over
+     */
     public BooleanProperty isGameOver() {
         return gameOver;
     }
 
+    /**
+     * Sets the boolean value of GameOver.
+     * @param gameOver the new boolean value of game over
+     */
     public void setGameOver(boolean gameOver) {
         this.gameOver.set(gameOver);
     }
 
-    public BooleanProperty displayAddBallTextProperty() {
+    /**
+     * Gets the boolean property of displayAddBallText.
+     * @return the boolean property of the whether the add ball text is display
+     */
+    public BooleanProperty getDisplayAddBallText() {
         return displayAddBallText;
     }
 
+    /**
+     * Sets the boolean value of displayAddBallText
+     * @param displayAddBallText the new boolean value of whether the add ball text is display
+     */
     public void setDisplayAddBallText(boolean displayAddBallText) {
         this.displayAddBallText.set(displayAddBallText);
     }
