24ba3c123f0b888a8546174596ec67c2f6097ce5
clarine24 <tan.ckaili01@gmail.com>
2021-12-11 23:40:27 +0800

    Add JavaDoc

--- src/main/java/com/game/BrickDestroy/ClayBrickModel.java
+++ src/main/java/com/game/BrickDestroy/ClayBrickModel.java
@@ -2,10 +2,21 @@ package com.game.BrickDestroy;
 
 import javafx.scene.shape.Rectangle;
 
+/**
+ * The ClayBrickModel class is the clay brick model.
+ * This class extends the BrickModel class.
+ * @author Clarine Tan Kaili (20194533)
+ * @version 2.0
+ * @since 11/12/21
+ */
 public class ClayBrickModel extends BrickModel {
     private static final String NAME = "clayBrick";
     private static final int FULL_STRENGTH = 1;
 
+    /**
+     * Creates a new instance of ClayBrickModel.
+     * @param brick the brick to be created
+     */
     public ClayBrickModel(Rectangle brick) {
         super(brick, NAME, FULL_STRENGTH);
     }
