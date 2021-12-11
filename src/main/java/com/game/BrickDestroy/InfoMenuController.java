483b2fdbc213665ee5c43bb09f5bf3d98c485a08
clarine24 <tan.ckaili01@gmail.com>
2021-12-11 21:21:04 +0800

    Change display info text

--- src/main/java/com/game/BrickDestroy/InfoMenuController.java
+++ src/main/java/com/game/BrickDestroy/InfoMenuController.java
@@ -5,28 +5,12 @@ import javafx.scene.control.Label;
 
 import java.io.IOException;
 
+
 public class InfoMenuController {
-    @FXML private Label title;
-    @FXML private Label content1;
-    @FXML private Label content2;
-    @FXML private Label content3;
-    @FXML private Label content4;
-    @FXML private Label content5;
-    @FXML private Label content6;
-    @FXML private Label content7;
-
-    @FXML
-    private void initialize() {
-        title.setText("How to Play");
-        content1.setText("GOAL: Destroy all Bricks!");
-        content2.setText("Press Space to Start/Pause Game");
-        content3.setText("Press A Key to Move Left");
-        content4.setText("Press D Key to Move Right");
-        content5.setText("Press ESC Key to Enter/Exit Pause Menu");
-        content6.setText("Press ALT+SHIFT+F1 Key to Open Console");
-        content7.setText("Game automatically pause when window\nloses focus");
-    }
-
+    /**
+     * Switch scene to home menu.
+     * @throws IOException
+     */
     @FXML
     private void closeButtonClicked() throws IOException {
         Stages.setRoot("HomeMenuView");
