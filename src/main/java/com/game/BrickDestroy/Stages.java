cbdc4d7a7b378398db9cf5477c00f160e7265c27
clarine24 <tan.ckaili01@gmail.com>
2021-12-11 19:34:38 +0800

    Create JavaDoc

--- src/main/java/com/game/BrickDestroy/Stages.java
+++ src/main/java/com/game/BrickDestroy/Stages.java
@@ -11,21 +11,60 @@ import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
+/**
+ * The Stages class contains all the stages of the application.
+ * @author Clarine Tan Kaili (20194533)
+ * @version 2.0
+ * @since 11/12/21
+ */
 public class Stages {
-    private static final String DEF_TITLE = "Brick Destroy";
+    /**
+     * The default title of homeStage and gameStage.
+     */
+    private static final String GAME_TITLE = "Brick Destroy";
+    /**
+     * The title of debugStage.
+     */
     private static final String DEBUG_TITLE = "Debug Console";
 
+    /**
+     * The home menu stage.
+     */
     private Stage homeStage;
+    /**
+     * The game board stage.
+     */
     private Stage gameStage;
+    /**
+     * The debug panel stage.
+     */
     private Stage debugStage;
 
+    /**
+     * The scene on the stage.
+     */
     private static Scene scene;
 
+    /**
+     * The created instance of Stages.
+     */
     private static Stages instance;
 
+    /**
+     * The loaded object.
+     */
     private Parent root;
+    /**
+     * The loaded game board fxml.
+     */
     private Parent gameRoot;
 
+    /**
+     * Creates an instance of Stages with the primary stage being the homeStage.
+     * Creates empty stage for all stages in the application.
+     * Set the style and modality for all stages.
+     * @param stage the primary stage
+     */
     public Stages(Stage stage) {
         homeStage = stage;
         homeStage.initStyle(StageStyle.UNDECORATED); //remove window decoration
@@ -40,10 +79,20 @@ public class Stages {
         instance = this;
     }
 
+    /**
+     * Get the created instance of Stages
+     * @return the Stages instance
+     */
     public static Stages getInstance() {
         return instance;
     }
 
+    /**
+     * Initialise the stage.
+     * Set the scene on the stage and show the stage.
+     * Set the focus to be on the stage.
+     * @param stage the stage name
+     */
     private void initialize(Stage stage) {
         scene = new Scene(root);
         stage.setScene(scene);
@@ -52,20 +101,31 @@ public class Stages {
         setFocus();
     }
 
+    /**
+     * Display the home menu.
+     * Hides the game board.
+     * @throws IOException
+     */
     public void homeStage() throws IOException {
         gameStage.hide();
         root = loadFXML("HomeMenuView");
-        homeStage.setTitle(DEF_TITLE);
+        homeStage.setTitle(GAME_TITLE);
         initialize(homeStage);
         setLocation(homeStage);
         homeStage.setOnCloseRequest(e -> onCloseGame());
     }
 
+    /**
+     * Display the game board.
+     * Hides the home menu.
+     * Informs the game board model when the stage loses focus.
+     * @throws IOException
+     */
     public void gameStage() throws IOException {
         homeStage.hide();
         gameRoot = loadFXML("GameBoardView");
         root = gameRoot;
-        gameStage.setTitle(DEF_TITLE);
+        gameStage.setTitle(GAME_TITLE);
         initialize(gameStage);
         setLocation(gameStage);
         gameStage.focusedProperty().addListener((observableValue, oldValue, newValue) -> {
@@ -76,6 +136,10 @@ public class Stages {
         gameStage.setOnCloseRequest(e -> onCloseGame());
     }
 
+    /**
+     * Display the debug panel.
+     * @throws IOException
+     */
     public void debugConsole() throws IOException {
         root = loadFXML("DebugConsoleView");
         debugStage.setTitle(DEBUG_TITLE);
@@ -84,15 +148,30 @@ public class Stages {
         debugStage.setOnCloseRequest(event -> root = gameRoot);
     }
 
+    /**
+     * Switch scene.
+     * @param fxml the fxml file name without the file extension
+     * @throws IOException
+     */
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
+    /**
+     * Load a fxml file.
+     * @param fxml the fxml file name without the file extension
+     * @return the loaded file
+     * @throws IOException
+     */
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Stages.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
+    /**
+     * Set the location of stage (homeStage or gameStage).
+     * @param stage the stage name
+     */
     private void setLocation(Stage stage) {
         Rectangle2D primaryScreen = Screen.getPrimary().getVisualBounds();
 
@@ -100,6 +179,10 @@ public class Stages {
         stage.setY((primaryScreen.getHeight() - stage.getHeight()) / 2);
     }
 
+    /**
+     * Set the location of debug console.
+     * Debug panel will be placed at the center of the game stage window.
+     */
     private void setDebugLocation() {
         double x = (gameStage.getWidth() - debugStage.getWidth()) / 2 + gameStage.getX();
         double y = (gameStage.getHeight() - debugStage.getHeight()) / 2 + gameStage.getY();
@@ -108,10 +191,16 @@ public class Stages {
         debugStage.setY(y);
     }
 
+    /**
+     * Set focus on stage.
+     */
     public void setFocus() {
         root.requestFocus();
     }
 
+    /**
+     * Closes open external txt files when game window is closed.
+     */
     public void onCloseGame() {
         GameBoardModel.getInstance().getGameOverModel().getScoreBoardModel().closeAllFiles();
     }
