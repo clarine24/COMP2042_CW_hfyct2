e7402aa314b18c495bed1b0fb0a958b42c1b7e65
clarine24 <tan.ckaili01@gmail.com>
2021-12-11 19:39:53 +0800

    Create JavaDoc

--- src/main/java/com/game/BrickDestroy/HomeMenuController.java
+++ src/main/java/com/game/BrickDestroy/HomeMenuController.java
@@ -4,18 +4,38 @@ import javafx.fxml.FXML;
 
 import java.io.IOException;
 
+/**
+ * The HomeMenuController class is the controller for home menu view.
+ * @author Clarine Tan Kaili (20194533)
+ * @version 2.0
+ * @since 11/12/21
+ */
 public class HomeMenuController {
+    /**
+     * Show the game stage.
+     * Method called when start button is clicked.
+     * @throws IOException
+     */
     @FXML
     private void startButtonClicked() throws IOException {
         Stages stages = Stages.getInstance();
         stages.gameStage();
     }
 
+    /**
+     * Switch scene to info menu view.
+     * Method called when info button is clicked.
+     * @throws IOException
+     */
     @FXML
     private void infoButtonClicked() throws IOException {
         Stages.setRoot("InfoMenuView");
     }
 
+    /**
+     * Exit the program.
+     * Method called when the exit button is clicked.
+     */
     @FXML
     private void exitButtonClicked() {
         System.out.println("Goodbye " + System.getProperty("user.name"));
