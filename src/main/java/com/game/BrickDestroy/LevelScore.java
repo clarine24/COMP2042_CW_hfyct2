032e059f810579a4b5d8b5f2a73d1cfb1398d2c2
clarine24 <tan.ckaili01@gmail.com>
2021-12-12 12:44:47 +0800

    Add JavaDoc

--- src/main/java/com/game/BrickDestroy/LevelScore.java
+++ src/main/java/com/game/BrickDestroy/LevelScore.java
@@ -5,6 +5,12 @@ import javafx.beans.property.SimpleIntegerProperty;
 
 import java.io.*;
 
+/**
+ * The LevelScore calculates and stores all the scores.
+ * @author Clarine Tan Kaili (20194533)
+ * @version 2.0
+ * @since 11/12/21
+ */
 public class LevelScore {
     private static LevelScore instance;
 
@@ -21,6 +27,10 @@ public class LevelScore {
     private IntegerProperty[] highScores;
     private final int TOP_SCORES= 5;
 
+    /**
+     * Creates a new instance of LevelScore for the given level.
+     * @param level the current level
+     */
     public LevelScore(int level) {
         totalScore = new SimpleIntegerProperty(0);
 
@@ -31,10 +41,18 @@ public class LevelScore {
         instance = this;
     }
 
+    /**
+     * Gets the created LevelScore instance.
+     * @return the LevelScore instance
+     */
     public static LevelScore getInstance() {
         return instance;
     }
 
+    /**
+     * Creates a new txt file, if it does not exist, to store the scores for the given level.
+     * @param level the level
+     */
     private void createFile(int level) {
         scoreBoardFile = new File(FILE_PATH + "Level" + level + ".txt");
         try {
@@ -50,6 +68,10 @@ public class LevelScore {
         }
     }
 
+    /**
+     * Calculates the total score and stores the score in the txt file.
+     * Calculates the high scores.
+     */
     public void calculateTotalScore() {
         totalScore.set(totalScore.get() + ballScore());
 
@@ -64,6 +86,10 @@ public class LevelScore {
         calculateTopScores();
     }
 
+    /**
+     * Saves the given score in the txt file.
+     * @param score the score
+     */
     private void writeToFile(int score) {
         try {
             long lastLine = file.length();
@@ -76,6 +102,9 @@ public class LevelScore {
         }
     }
 
+    /**
+     * Calculates the top 5 scores.
+     */
     private void calculateTopScores() {
         int scoreCount = numberOfScoreInFile();
         createHighScoreArray(scoreCount);
@@ -83,6 +112,10 @@ public class LevelScore {
         saveHighScoresInArray(tmp);
     }
 
+    /**
+     * Gets the number of scores saved in the file.
+     * @return the number of scores saved in the file
+     */
     private int numberOfScoreInFile() {
         int count = 0;
 
@@ -104,6 +137,13 @@ public class LevelScore {
         return count;
     }
 
+    /**
+     * Creates an empty array to save the top 5 scores.
+     * The size of the empty array depends on the number of scores saved in the txt file.
+     * If there is at least 5 saved scores, the size of the array will be 5.
+     * Otherwise, the size of array will be the same as the number of saved scores.
+     * @param scoreCount the number of scores
+     */
     private void createHighScoreArray(int scoreCount) {
         if(scoreCount < 5) {
             highScores = new SimpleIntegerProperty[scoreCount];
@@ -117,6 +157,10 @@ public class LevelScore {
         }
     }
 
+    /**
+     * Gets the pointers to the top 5 scores in the txt file.
+     * @return the pointers to the top scores in the txt file
+     */
     private long[] getTopScoresPointer() {
         long[] tmp = new long[highScores.length];
         long previousPointer;
@@ -180,6 +224,10 @@ public class LevelScore {
         return tmp;
     }
 
+    /**
+     * Saves the top 5 scores in the array of high scores with the given array of pointer.
+     * @param tmp the pointers to the top 5 scores in the txt file
+     */
     private void saveHighScoresInArray(long[] tmp) {
         for(int i=0; i<highScores.length; i++) {
             try {
@@ -192,39 +240,70 @@ public class LevelScore {
         }
     }
 
+    /**
+     * Gets the score obtained from the remaining ball count.
+     * @return the score obtained from the ball count
+     */
     private int ballScore() {
         int remainingBallCount = GameBoardModel.getInstance().getWallModel().getBallCount().get();
         return remainingBallCount * 50; //each ball left is 50 points
     }
 
+    /**
+     * Calculates the total score obtained from the bricks by adding the given brick score.
+     * @param addScore the score to be added
+     */
     private void totalBrickScore(int addScore) {
         totalScore.set(totalScore.get() + addScore);
     }
 
+    /**
+     * Adds the clay brick score to the total brick score.
+     */
     public void clayBrickScore() {
         totalBrickScore(CLAY_BRICK_SCORE);
     }
 
+    /**
+     * Adds the cement brick score to the total brick score.
+     */
     public void cementBrickScore() {
         totalBrickScore(CEMENT_BRICK_SCORE);
     }
 
+    /**
+     * Adds the steel brick score to the total brick score.
+     */
     public void steelBrickScore() {
         totalBrickScore(STEEL_BRICK_SCORE);
     }
 
+    /**
+     * Resets the total score to 0.
+     */
     public void resetScore() {
         totalScore.set(0);
     }
 
+    /**
+     * Gets the integer property of totalScore.
+     * @return the integer property of the total score
+     */
     public IntegerProperty getTotalScore() {
         return totalScore;
     }
 
+    /**
+     * Gets the integer property of the array of highScores.
+     * @return the array of integer property of high scores
+     */
     public IntegerProperty[] getHighScores() {
         return highScores;
     }
 
+    /**
+     * Close the txt file.
+     */
     public void closeFile() {
         try {
             file.close();
