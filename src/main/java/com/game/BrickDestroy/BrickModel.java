ca03f0623851041e2e5b5be9125c1022d2bd6d6d
clarine24 <tan.ckaili01@gmail.com>
2021-12-12 18:02:29 +0800

    Add JavaDoc

--- src/main/java/com/game/BrickDestroy/BrickModel.java
+++ src/main/java/com/game/BrickDestroy/BrickModel.java
@@ -6,6 +6,12 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * The BrickModel class is the brick model.
+ * @author Clarine Tan Kaili (20194533)
+ * @version 2.0
+ * @since 11/12/21
+ */
 public class BrickModel {
     private Rectangle brickFace;
     private final StringProperty name;
@@ -17,6 +23,12 @@ public class BrickModel {
 
     private boolean additionalBall;
 
+    /**
+     * Creates a new instance of BrickModel with the given brick's shape, name and full strength.
+     * @param brick the shape of the brick
+     * @param name the name of the brick
+     * @param fullStrength the full strength of the brick
+     */
     public BrickModel(Rectangle brick, String name, int fullStrength) {
         brickFace = makeBrickFace(brick);
         this.name = new SimpleStringProperty(name);
@@ -26,41 +38,80 @@ public class BrickModel {
         setAdditionalBall(false);
     }
 
+    /**
+     * Creates the brick shape based on the given brick shape.
+     * @param brick the shape of the brick
+     * @return the created brick shape
+     */
     protected Rectangle makeBrickFace(Rectangle brick) {
         return new Rectangle(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
     }
 
+    /**
+     * Gets the shape of the brick.
+     * @return the brick shape
+     */
     public Rectangle getBrickFace() {
         return brickFace;
     }
 
+    /**
+     * Sets the impact on the brick.
+     * @return the boolean value of the property broken
+     */
     public boolean setImpact() {
         impact();
         return isBroken().get();
     }
 
+    /**
+     * Sets the effect of the impact of the brick.
+     * Reduces the strength of the brick and sets the value of the boolean broken.
+     */
     public void impact() {
         currentStrength--;
         broken.set(currentStrength == 0);
     }
 
+    /**
+     * Resets the state of the brick.
+     * Restores the full strength of the brick.
+     */
     public void repair() {
         broken.set(false);
         currentStrength = fullStrength;
     }
 
+    /**
+     * Gets the string property of the brick's name.
+     * @return the string property of name
+     */
     public StringProperty getName() {
         return name;
     }
 
+    /**
+     * Gets the boolean property of broken.
+     * Check if the brick is broken.
+     * @return the value of boolean property broken
+     */
     public BooleanProperty isBroken() {
         return broken;
     }
 
+    /**
+     * Gets the boolean value of additionalBall.
+     * Checks if the brick contains an additional ball.
+     * @return the boolean value of additional ball
+     */
     public boolean isAdditionalBall() {
         return additionalBall;
     }
 
+    /**
+     * Sets the value of the boolean additionalBall.
+     * @param additionalBall the new value of the boolean additional ball
+     */
     public void setAdditionalBall(boolean additionalBall) {
         this.additionalBall = additionalBall;
     }
